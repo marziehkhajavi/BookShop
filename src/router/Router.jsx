f8b0c21e7246e2d62674a1c370e97fd23828c726
Marzieh Khajavi <marziehkhjvi@gmail.com>
2025-05-02 11:53:51 +0330

    Router.jsx changed

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import AuthPage from 'src/pages/AuthPage';
 import HomePage from 'src/pages/HomePage';
@@ -11,18 +11,37 @@ import Loader from 'src/components/modules/Loader';
 
 const Router = () => {
     const {data, isLoading, error} = useQuery({queryKey: ["books"], queryFn: getBooks});
-    const token = document.cookie;
+    const [token, setToken] = useState("");
+
+    // const token = document.cookie;
+    // console.log(token)
+    useEffect(() => {
+        const getTokenFromCookie = () => {
+            const cookieToken = document.cookie
+                .split('; ')
+                .find(row => row.startsWith('token='))
+                ?.split('=')[1];
+            setToken(cookieToken || "");
+        };
+
+        getTokenFromCookie();
+        const interval = setInterval(getTokenFromCookie, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
     if (isLoading) return <Loader />
     return (
         <Routes>
             <Route index element={<HomePage />} />
-            <Route path="/auth" 
-                    element={!token ? <AuthPage /> :
-                                      <Navigate to="/" /> } />
+            <Route path="/auth" element={<AuthPage />} />
+            {/* <Route path="/auth"
+                    element= {!token ? <AuthPage /> :
+                                      <Navigate to="/" /> } /> */}
             <Route path="/dashboard" 
-                   element={ token ? <BooksList /> :
+                    element={ token ? <BooksList /> :
                                      <Navigate to="/auth" replace />} />
+            {/* <Route path="/dashboard" 
+                   element={<BooksList />} /> */}
             <Route path='/book/:id' element={<DetailsPage />} />
         </Routes>
     );
